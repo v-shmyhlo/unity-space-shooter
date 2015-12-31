c6e1659c36bae0c6cd13d0f386d0c1bc1b590f10
unknown <aion.stu@gmail.com>
2015-12-31 20:59:08 +0200

    initial commit

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+	public float tumble;
+
+	void Start () {
+		Rigidbody rigidbody = gameObject.GetComponent<Rigidbody> ();
+		rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+	}
+}
